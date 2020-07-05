c29b463bdd84b8a4f379ce7e3c9fdc4454877182
oitsjustjose <stovallj1995@gmail.com>
2020-07-05 09:29:42 -0500

    Honestly configs don't need names

--- src/app/models/config.model.ts
+++ src/app/models/config.model.ts
@@ -2,12 +2,10 @@ import { Document, model, Schema, Types } from 'mongoose'
 
 const ConfigSchema = new Schema({
     json: Object,
-    name: String,
 })
 
 export type ConfigModel = Document & {
     json: object,
-    name: string,
 }
 
 export default model<ConfigModel>('configs', ConfigSchema, 'configs')
